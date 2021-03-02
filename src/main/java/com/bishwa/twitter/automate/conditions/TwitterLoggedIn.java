ac34e4e9c2040d04ff9af90c497258c4f00fa120
Bishwa Poudel <bispoul181@gmail.com>
2021-03-03 00:01:07 +0545

    Fix login error

--- src/main/java/com/bishwa/twitter/automate/conditions/TwitterLoggedIn.java
+++ src/main/java/com/bishwa/twitter/automate/conditions/TwitterLoggedIn.java
@@ -1,10 +1,8 @@
 package com.bishwa.twitter.automate.conditions;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import java.util.List;
@@ -19,12 +17,7 @@ public class TwitterLoggedIn implements ExpectedCondition<Boolean> {
 
     @Override
     public Boolean apply(WebDriver driver) {
-        List<WebElement> menu = Objects.requireNonNull(driver).findElements(By.xpath("//a[@data-testid=\"SideNav_AccountSwitcher_Button\"]"));
-
-        if(menu.isEmpty()) {
-            Actions actions = new Actions(driver);
-            actions.sendKeys(Keys.F5).perform();
-        }
+        List<WebElement> menu = Objects.requireNonNull(driver).findElements(By.xpath("//div[@data-testid=\"SideNav_AccountSwitcher_Button\"]"));
 
         return !menu.isEmpty();
     }
