fc534396c9ee91476d750a87a3bb1fe8c493c260
Bishwa Poudel <bispoul181@gmail.com>
2021-03-02 23:45:24 +0545

    Refresh once on twitter login condition

--- src/main/java/com/bishwa/twitter/automate/conditions/TwitterLoggedIn.java
+++ src/main/java/com/bishwa/twitter/automate/conditions/TwitterLoggedIn.java
@@ -1,8 +1,10 @@
 package com.bishwa.twitter.automate.conditions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import java.util.List;
@@ -17,7 +19,12 @@ public class TwitterLoggedIn implements ExpectedCondition<Boolean> {
 
     @Override
     public Boolean apply(WebDriver driver) {
-        List<WebElement> menu = Objects.requireNonNull(driver).findElements(By.xpath("//a[@data-testid=\"AppTabBar_Home_Link\"]"));
+        List<WebElement> menu = Objects.requireNonNull(driver).findElements(By.xpath("//a[@data-testid=\"SideNav_AccountSwitcher_Button\"]"));
+
+        if(menu.isEmpty()) {
+            Actions actions = new Actions(driver);
+            actions.sendKeys(Keys.F5).perform();
+        }
 
         return !menu.isEmpty();
     }
