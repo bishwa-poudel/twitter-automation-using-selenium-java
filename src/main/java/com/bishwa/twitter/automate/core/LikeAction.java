d7ec906aab67789efecc8e97b7fd03f2743b5eb3
Bishwa Poudel <bispoul181@gmail.com>
2021-03-03 00:41:43 +0545

    Fix url and enable click

--- src/main/java/com/bishwa/twitter/automate/core/LikeAction.java
+++ src/main/java/com/bishwa/twitter/automate/core/LikeAction.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 /**
@@ -29,18 +30,18 @@ public class LikeAction {
     private final Wait<WebDriver> fluentWait = IDriverManager.getFluentWait();
     private final JavascriptExecutor js = IDriverManager.getJSExecutor();
 
-    private static final String TWITTER_HASHTAG_URL = "https://twitter.com/hashtag/100DaysOfCode";
+    private static final String TWITTER_HASHTAG_URL = "https://twitter.com/hashtag/100DaysOfCode?f=live";
     private static final String TWITTER_ADVANCED_URL = "https://twitter.com/search?q=(Day)%20-Visit%20(%23100DaysOfCode%20OR%20%23javascript%20OR%20%23CodeNewbie%20OR%20%23DEVCommunity%20OR%20%23CodeNewbies%20OR%20%23WomenWhoCode%20OR%20%23programming%20OR%20%23java)%20lang%3Aen%20-filter%3Alinks%20-filter%3Areplies&src=typed_query&f=live";
     private final Integer LIKE_LIMIT = 10;
 
     public void action() {
-        driver.get(TWITTER_ADVANCED_URL);
+        driver.get(TWITTER_HASHTAG_URL);
         fluentWait.until(twitterLoggedIn);
 
         IntStream.rangeClosed(1, 30).forEach(iter -> {
             fetchTweetLikeElements().forEach(el -> {
                 js.executeScript("arguments[0].scrollIntoView();", el);
-//                js.executeScript("arguments[0].click();", el);
+                js.executeScript("arguments[0].click();", el);
 
                 try {
                     Thread.sleep(1000);
